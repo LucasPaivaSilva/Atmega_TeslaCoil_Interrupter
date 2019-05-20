0a3f07311eaced204aa442f37c23d7d965ef496e
Lucas Paiva da Silva <lucaspaiva.luc@gmail.com>
2019-05-20 10:48:23 -0300

    midi script atualizado para L e D

--- PythonProgram/MidiToSerial.py
+++ PythonProgram/MidiToSerial.py
@@ -4,8 +4,12 @@ Receive messages from the input port and print them out.
 from __future__ import print_function
 import sys
 import mido
+import serial
+import datetime
 data = ''
 
+ser = serial.Serial('COM7', 9600)
+ser.timeout = 0.01
 if len(sys.argv) > 1:
     portname = sys.argv[1]
 else:
@@ -18,11 +22,12 @@ try:
         for message in port:
             print('Received {}'.format(message))
             if message.velocity == 0:
-            	data = '1'
+            	data = 'D'
             else:
-            	data = '0'
+            	data = 'L'
             data = data + str(message.note)
             print(data)
+            ser.write(data.encode())
             sys.stdout.flush()
 except KeyboardInterrupt:
     pass
